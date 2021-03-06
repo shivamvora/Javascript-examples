c6bffe933ae029ced2cacbc283be010fe6931b64
shivamvora <shivampatel27102001@gmail.com>
2021-03-06 22:53:51 +0530

    javascript part done

---
+++ countdown-timer/script.js
@@ -0,0 +1,27 @@
+const daysEl = document.getElementById( 'days' );
+const hoursEl = document.getElementById( 'hours' );
+const minsEl = document.getElementById( 'mins' );
+const secondsEl = document.getElementById( 'seconds' );
+
+const newYears = '1 Jan 2022';
+
+function countdown() {
+    const newYearsDate = new Date( newYears );
+    const currentDate = new Date();
+
+    const totalSeconds = ( newYearsDate - currentDate ) / 1000;
+
+    const days = Math.floor( totalSeconds / 3600 / 24 );
+    const hours = Math.floor( totalSeconds / 3600 ) % 24;
+    const mins = Math.floor( totalSeconds / 60 ) % 60;
+    const seconds = Math.floor( totalSeconds ) % 60;
+
+    daysEl.innerHTML= days;
+    hoursEl.innerHTML= hours;
+    minsEl.innerHTML= mins;
+    secondsEl.innerHTML= seconds;
+
+}
+countdown();
+
+setInterval( countdown, 1000 );
